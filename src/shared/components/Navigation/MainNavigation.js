a58e6193d5246faed972efbf7115f5f10f083eda
jiyoung-dev <janice10508@gmail.com>
2024-02-10 23:06:36 +0900

    update: 사이드바 애니메이션 적용 - react-transition-group 라이브러리 설치 - CSSTransition 컴포넌트 사용하여 자동 슬라이드 효과 적용

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -10,28 +10,26 @@ import './MainNavigation.css';
 const MainNavigation = (props) => {
     const [drawerIsOpen, setDrawerIsOpen] = React.useState(false);
 
-    const openDrawer = () => {
+    const openDrawerHandler = () => {
         setDrawerIsOpen(true);
     };
 
-    const closeDrawer = () => {
+    const closeDrawerHandler = () => {
         setDrawerIsOpen(false);
     };
 
     return (
         <>
-            {drawerIsOpen ? <Backdrop onClick={closeDrawer} /> : null}
-            {drawerIsOpen ? (
-                <SideDrawer>
-                    <nav className="main-navigation__drawer-nav">
-                        <NavLinks />
-                    </nav>
-                </SideDrawer>
-            ) : null}
+            {drawerIsOpen ? <Backdrop onClick={closeDrawerHandler} /> : null}
+            <SideDrawer show={drawerIsOpen} onClick={closeDrawerHandler}>
+                <nav className="main-navigation__drawer-nav">
+                    <NavLinks />
+                </nav>
+            </SideDrawer>
             <MainHeader>
                 <button
                     className="main-navigation__menu-btn"
-                    onClick={openDrawer}>
+                    onClick={openDrawerHandler}>
                     <span />
                     <span />
                     <span />
