f2a532ed719927aa689d1b1f80da83b4a6a3a6df
jiyoung-dev <janice10508@gmail.com>
2024-02-10 20:55:38 +0900

    update: NavLinks 컴포넌트 추가 - NavLink 컴포넌트 모음 - nav 영역은 데스크톱 모드일때만 보여지도록

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,8 +1,9 @@
 import React from 'react';
-
-import './MainNavigation.css';
-import MainHeader from './MainHeader';
 import { Link } from 'react-router-dom';
+
+import MainHeader from './MainHeader';
+import NavLinks from './NavLinks';
+import './MainNavigation.css';
 
 const MainNavigation = (props) => {
     return (
@@ -15,7 +16,9 @@ const MainNavigation = (props) => {
             <h1 className="main-navigation__title">
                 <Link to="/">Spot Mate</Link>
             </h1>
-            <nav>...</nav>
+            <nav className="main-navigation__header-nav">
+                <NavLinks />
+            </nav>
         </MainHeader>
     );
 };
