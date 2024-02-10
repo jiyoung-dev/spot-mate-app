a980dd3ba03b5cba052875a8fe4dd9b480758ef3
jiyoung-dev <janice10508@gmail.com>
2024-02-10 22:18:37 +0900

    update: 사이드바 구현 - 사이드바 상태 다루기 - Backdrop) 사이드바 close 이벤트 수신할 컴포넌트

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -3,23 +3,47 @@ import { Link } from 'react-router-dom';
 
 import MainHeader from './MainHeader';
 import NavLinks from './NavLinks';
+import Backdrop from '../UIElements/Backdrop';
+import SideDrawer from './SideDrawer';
 import './MainNavigation.css';
 
 const MainNavigation = (props) => {
+    const [drawerIsOpen, setDrawerIsOpen] = React.useState(false);
+
+    const openDrawer = () => {
+        setDrawerIsOpen(true);
+    };
+
+    const closeDrawer = () => {
+        setDrawerIsOpen(false);
+    };
+
     return (
-        <MainHeader>
-            <button className="main-navigation__menu-btn">
-                <span />
-                <span />
-                <span />
-            </button>
-            <h1 className="main-navigation__title">
-                <Link to="/">Spot Mate</Link>
-            </h1>
-            <nav className="main-navigation__header-nav">
-                <NavLinks />
-            </nav>
-        </MainHeader>
+        <>
+            {drawerIsOpen ? <Backdrop onClick={closeDrawer} /> : null}
+            {drawerIsOpen ? (
+                <SideDrawer>
+                    <nav className="main-navigation__drawer-nav">
+                        <NavLinks />
+                    </nav>
+                </SideDrawer>
+            ) : null}
+            <MainHeader>
+                <button
+                    className="main-navigation__menu-btn"
+                    onClick={openDrawer}>
+                    <span />
+                    <span />
+                    <span />
+                </button>
+                <h1 className="main-navigation__title">
+                    <Link to="/">Spot Mate</Link>
+                </h1>
+                <nav className="main-navigation__header-nav">
+                    <NavLinks />
+                </nav>
+            </MainHeader>
+        </>
     );
 };
 
