a9efb2eca4dd6f6b1dd9c895c067310ba396787d
Jiyoung <janice10508@gmail.com>
2024-02-21 20:55:10 +0900

    refactor: 코드 포맷팅 - 줄바꿈, 들여쓰기 수정

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -4,11 +4,12 @@ import Input from '../../shared/components/FormElements/Input';
 import './NewPlace.css';
 
 const NewPlace = () => {
-  return (
-    <form className="place-form">
-      <Input element="input" type="text" label="Title" />
-    </form>
-  );
+    return (
+        <form className="place-form">
+            <Input element="input" type="text" label="Title" />
+            <Input element="input" type="text" label="Description" />
+        </form>
+    );
 };
 
 export default NewPlace;
