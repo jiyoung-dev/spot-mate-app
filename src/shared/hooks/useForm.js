93ac052a5fe7a850087056672ac87e9b711ef6df
Jiyoung <janice10508@gmail.com>
2024-02-29 17:51:35 +0900

    update: 커스텀훅 useForm 추가 - NewPlace의 폼 상태관리 코드 > useForm으로 이관

---
+++ src/shared/hooks/useForm.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+const formReducer = (state, action) => {
+    switch (action.type) {
+        case 'INPUT_CHANGE':
+            let formIsValid = true;
+            for (const inputId in state.inputs) {
+                if (inputId === action.inputId) {
+                    formIsValid = formIsValid && action.isValid;
+                } else {
+                    formIsValid = formIsValid && state.inputs[inputId].isValid;
+                }
+            }
+            return {
+                ...state,
+                inputs: {
+                    ...state.inputs,
+                    [action.inputId]: {
+                        value: action.value,
+                        isValid: action.isValid,
+                    },
+                },
+                isValid: formIsValid,
+            };
+        default:
+            return state;
+    }
+};
+
+const useForm = (inputValues) => {
+    const [formState, dispatch] = React.useReducer(formReducer, {
+        inputs: inputValues,
+        isValid: false,
+    });
+
+    const inputHandler = React.useCallback((id, value, isValid) => {
+        dispatch({
+            type: 'INPUT_CHANGE',
+            value: value,
+            isValid: isValid,
+            inputId: id,
+        });
+    }, []);
+
+    return [formState, inputHandler];
+};
+
+export default useForm;
