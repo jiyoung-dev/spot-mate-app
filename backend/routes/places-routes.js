1b5df0000ae9d4c8b8eba1f3ecbb2c4dc0a205b7
jiyoung-dev <janice10508@gmail.com>
2024-03-30 00:35:31 +0900

    add: Controller 추가 - 미들웨어 함수 로직만 작성할 컨트롤러 파일 생성 - routes.js 파일에서는 url 매핑되는 라우팅 정보만 다룬다 🧐 파일마다 집중하는 부분이 반드시 있도록

--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -1,54 +1,13 @@
 const express = require('express');
-const HttpError = require('../models/http-error');
 
 const router = express.Router();
 
-const DUMMY_PLACES = [
-	{
-		id: 'p1',
-		title: 'Empire State Building',
-		description: 'One of the most famous sky scrapers in the world!',
-		location: {
-			lat: 40.7484474,
-			lng: -73.9871516,
-		},
-		address: '20 W 34th St, New York, NY 10001',
-		creator: 'u1',
-	},
-];
+const placesControllers = require('../controllers/places-controllers');
 
 // 장소ID에 해당하는 장소정보를 반환
-router.get('/:pid', (req, res, next) => {
-	const placeId = req.params.pid;
-	const place = DUMMY_PLACES.find(p => {
-		return p.id === placeId;
-	});
-
-	if (!place) {
-		// const error = new Error('Could not find a place for the provided id.');
-		// error.code = 404;
-		// return next(error);
-		throw new HttpError('Could not find a place for the provided id.', 404);
-	}
-
-	res.json({ place }); // => { place } => { place: place }
-});
+router.get('/:pid', placesControllers.getPlaceById);
 
 // 작성자ID가 등록한 장소를 반환
-router.get('/user/:uid', (req, res, next) => {
-	const userId = req.params.uid;
-	const place = DUMMY_PLACES.find(p => {
-		return p.creator === userId;
-	});
-
-	if (!place) {
-		throw new HttpError(
-			'Could not find a place for the provided user id.',
-			404
-		);
-	}
-
-	res.json({ place });
-});
+router.get('/user/:uid', placesControllers.getPlaceByUserId);
 
 module.exports = router;
