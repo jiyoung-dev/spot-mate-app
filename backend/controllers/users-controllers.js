fab2a718ea521608020ba80a47d8d4c63df67a77
jiyoung-dev <janice10508@gmail.com>
2024-04-11 21:16:38 +0900

    update: 회원가입 파라미터 수정, 중복가입 방지 로직추가

--- backend/controllers/users-controllers.js
+++ backend/controllers/users-controllers.js
@@ -1,25 +1,33 @@
+const { v4: uuid } = require('uuid');
+
+const HttpError = require('../models/http-error');
+
 const DUMMY_USERS = [
     {
         id: 'u1',
+        email: 'test@test.com',
         name: 'Jenny',
-        image: 'imageURL',
-        places: 5,
+        password: 'test123',
     },
 ];
 
 const getUsers = (req, res, next) => {
-    const users = DUMMY_USERS;
 
-    res.json({ users });
+    res.json({ users: DUMMY_USERS });
 }
 
 const signupUser = (req, res, next) => {
-    const { id, name, image, places } = req.body;
+    const { email, name, password } = req.body;
     const createdUser = {
-        id,
+        id: uuid(),
+        email,
         name,
-        image,
-        places
+        password
+    }
+
+    // 이미 존재하는 유저의 경우, 중복가입 방지 
+    if (DUMMY_USERS.find(user => user.email === email)) {
+        throw new HttpError('이미 사용중인 이메일입니다!', 401);
     }
 
     DUMMY_USERS.push(createdUser);
@@ -28,14 +36,16 @@ const signupUser = (req, res, next) => {
 }
 
 const loginUser = (req, res, next) => {
-    const loggedUserId = req.body.id;
-    // 유저리스트에 존재하는 id 만 로그인 허용 
-    const user = DUMMY_USERS.find(user => user.id === loggedUserId);
+    const { email, password } = req.body;
 
-    if (!user) {
-        res.status(404).json({ message: '일치하는 사용자가 없습니다.' });
+    // 유저리스트에 존재하는 id 만 로그인 허용 
+    const identifiedUser = DUMMY_USERS.find(user => user.email === email);
+
+    if (!identifiedUser || identifiedUser.password !== password) {
+        throw new HttpError('Could not identify user', 401);
     }
-    res.status(201).json({ loggedUser: user });
+    // user는 존재하는데, 비밀번호가 일치하지 않은경우 
+    res.status(201).json({ loggedUser: identifiedUser });
 }
 
 exports.getUsers = getUsers;
