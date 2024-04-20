eb39a7d4a7cf61245d98ad4acc08c209421e7241
jiyoung-dev <janice10508@gmail.com>
2024-04-20 18:02:46 +0900

    update: 장소 Mock data를 MongoDB 연동으로 대체

--- backend/controllers/places-controllers.js
+++ backend/controllers/places-controllers.js
@@ -1,23 +1,8 @@
-const { v4: uuid } = require("uuid");
 const { validationResult } = require("express-validator");
 
 const HttpError = require("../models/http-error");
 const getCoordsForAddress = require("../util/location");
 const Place = require("../models/place");
-
-let DUMMY_PLACES = [
-  {
-    id: "p1",
-    title: "Empire State Building",
-    description: "One of the most famous sky scrapers in the world!",
-    location: {
-      lat: 40.7484474,
-      lng: -73.9871516,
-    },
-    address: "20 W 34th St, New York, NY 10001",
-    creator: "u1",
-  },
-];
 
 const getPlaceById = async (req, res, next) => {
   const placeId = req.params.pid;
@@ -44,18 +29,24 @@ const getPlaceById = async (req, res, next) => {
 
 const getPlacesByUserId = async (req, res, next) => {
   const userId = req.params.uid;
-  const places = DUMMY_PLACES.find((p) => {
-    return p.creator === userId;
-  });
+  let places;
 
-  if (!places || places.length === 0) {
-    throw new HttpError(
-      "Could not find a place for the provided user id.",
-      404
-    );
+  try {
+    places = await Place.find({ creator: userId });
+  } catch (err) {
+    const error = new HttpError("Could not find a place.", 500);
+    return next(error);
   }
 
-  res.json({ places });
+  if (!places || places.length == 0) {
+    const error = new HttpError(
+      "Could not find a place for the provided user's id.",
+      404
+    );
+    return next(error);
+  }
+
+  res.json({ places: places });
 };
 
 const createPlace = async (req, res, next) => {
@@ -88,18 +79,16 @@ const createPlace = async (req, res, next) => {
   });
 
   try {
-    await createdPlace.save(); // db저장
+    await createdPlace.save();
   } catch (err) {
     const error = new HttpError("Creating place failed.", 500);
     return next(error);
   }
 
-  DUMMY_PLACES.push(createdPlace);
-
   res.status(201).json({ place: createdPlace });
 };
 
-const updatePlace = (req, res, next) => {
+const updatePlace = async (req, res, next) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
@@ -109,24 +98,32 @@ const updatePlace = (req, res, next) => {
   const placeId = req.params.pid;
   const { title, description } = req.body;
 
-  const updatedPlaceIndex = DUMMY_PLACES.findIndex(
-    (place) => place.id === placeId
-  );
+  try {
+    const updatedPlace = await Place.updateOne(
+      { _id: placeId },
+      { $set: { title: title, description: description } }
+    );
 
-  DUMMY_PLACES[updatedPlaceIndex].title = title;
-  DUMMY_PLACES[updatedPlaceIndex].description = description;
+    if (updatedPlace.nModified === 0) {
+      return res.status(404).json({ message: "Place not found." });
+    }
 
-  res.status(200).json({ message: "Place updated successfully." });
+    res.status(200).json({ message: "Place updated successfully." });
+  } catch (error) {
+    res.status(500).json({ message: "Updating place failed." });
+  }
 };
 
-const deletePlace = (req, res, next) => {
+const deletePlace = async (req, res, next) => {
   const placeId = req.params.pid;
 
-  // placeId와 일치하는 장소가 있으면 삭제
-  if (!DUMMY_PLACES.some((place) => place.id === placeId)) {
-    throw new new HttpError("Could not find a place for that id.", 404)();
+  try {
+    await Place.findByIdAndDelete(placeId);
+  } catch (err) {
+    const error = new HttpError("Could not find a place for that id.", 404);
+    return next(error);
   }
-  DUMMY_PLACES = DUMMY_PLACES.filter((place) => place.id !== placeId);
+
   res.status(200).json({ message: "Place deleted successfully" });
 };
 
